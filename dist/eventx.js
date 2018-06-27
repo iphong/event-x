644fc216ff13072781f10b4b5252eb935a25794c
Phong Vu <phong.vu@me.com>
2018-06-27 23:33:07 +0700

    First version

--- dist/eventx.js
+++ dist/eventx.js
@@ -1,20 +1,26 @@
 "use strict";
+/**
+ * @name: event-x
+ * @author: Phong Vu
+ */
 Object.defineProperty(exports, "__esModule", { value: true });
-const IDENTITY = Symbol('event-x');
+const LISTENERS = Symbol('event-x-listener');
 function listeners(target) {
-    if (!Reflect.has(target, '_listeners'))
-        Reflect.defineProperty(target, '_listener', { value: new Set() });
-    return Reflect.get(target, '_listeners');
+    if (!Reflect.has(target, LISTENERS))
+        Reflect.defineProperty(target, LISTENERS, { value: new Set() });
+    return Reflect.get(target, LISTENERS);
 }
 exports.listeners = listeners;
 function listen(target, type, callback) {
     const listener = { target, type, callback };
-    listener[IDENTITY] =
-        listeners(target).add(listener);
+    listeners(target).add(listener);
+    return function dispose() {
+        listeners(target).delete(listener);
+    };
 }
 exports.listen = listen;
 function emit(target, type, ...args) {
-    listeners(target).forEach((listener) => {
+    listeners(target).forEach(listener => {
         if (listener.type === type)
             Reflect.apply(listener.callback, target, args);
     });
--- src/eventx.ts
+++ src/eventx.ts
@@ -1,23 +1,26 @@
-const IDENTITY = Symbol('event-x')
-export type Listener = {
-  target: object
-  type: string
-  callback(): any
-}
+/**
+ * @name: event-x
+ * @author: Phong Vu
+ */
+
+const LISTENERS = Symbol('event-x-listener')
 export function listeners(target: object): Set<any> {
-  if (!Reflect.has(target, '_listeners'))
-    Reflect.defineProperty(target, '_listener', { value: new Set() })
-  return Reflect.get(target, '_listeners')
+    if (!Reflect.has(target, LISTENERS))
+        Reflect.defineProperty(target, LISTENERS, { value: new Set() })
+    return Reflect.get(target, LISTENERS)
 }
-export function listen(target: object, type: string, callback): void {
-  const listener: Listener = { target, type, callback }
-  listener[IDENTITY] =
-  listeners(target).add(listener)
+
+export function listen(target: object, type: string, callback): () => void {
+    const listener = { target, type, callback }
+    listeners(target).add(listener)
+    return function dispose() {
+        listeners(target).delete(listener)
+    }
 }
 
 export function emit(target: object, type: string, ...args): void {
-  listeners(target).forEach((listener: Listener) => {
-    if (listener.type === type)
-      Reflect.apply(listener.callback, target, args)
-  })
+    listeners(target).forEach(listener => {
+        if (listener.type === type)
+            Reflect.apply(listener.callback, target, args)
+    })
 }
